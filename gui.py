d5a1081140b05e1a6575967e9beaea0c64435886
robertalim <robert_alim@yahoo.com>
2023-12-01 22:50:12 +0800

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -3,10 +3,13 @@ import PySimpleGUI as Psg
 
 label = Psg.Text("Type in a todo")
 input_text = Psg.InputText(tooltip="Enter todo here", key="todo")
-button = Psg.Button("Add")
+button = Psg.Button("Add", key="Add")
+listbox = Psg.Listbox(values=get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_button = Psg.Button("Edit", key="Edit")
 
 window = Psg.Window("My To-Do App",
-                    layout=[[label], [input_text, button]],
+                    layout=[[label], [input_text, button], [listbox, edit_button]],
                     font=("Helvetica", 20))
 
 while True:
@@ -19,7 +22,21 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             write_todos(todos)
+            window["todos"].update(values=todos)
+            input_text.Update(value="")
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
 
+            todos = get_todos()
+            i = todos.index(todo_to_edit)
+            todos[i] = new_todo + "\n"
+
+            write_todos(todos)
+            window["todos"].update(values=todos)
+            input_text.Update(value="")
+        case "todos":
+            window["todo"].update(value=values['todos'][0])
         case Psg.WIN_CLOSED:
             break
 
